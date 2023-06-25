149a4a1bfed73fda2d71f5edda1932810a65317c
Adam Drewery <adam@drewery.uk>
2023-06-25 02:56:53 +0200

    use kartel dns label

--- infrastructure.tf
+++ infrastructure.tf
@@ -36,7 +36,7 @@ resource "azurerm_container_group" "kartel" {
   location            = azurerm_resource_group.kartel.location
   resource_group_name = azurerm_resource_group.kartel.name
   ip_address_type     = "Public"
-  dns_name_label      = "acitestcontainers"
+  dns_name_label      = "kartel"
   os_type             = "Linux"
 
   container {
